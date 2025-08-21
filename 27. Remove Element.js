91a8600208a632f2cf62956e5a3486703e26511c
Evg_S <nikelodiym1210@gmail.com>
2025-08-21 12:03:22 +0300

    27. Remove Element.js

---
+++ 27. Remove Element.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let k = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i]
+      k += 1
+    }
+  }
+  return k
+
+};
+console.log(removeElement([3, 2, 2, 3], 3));
+
+
+// Input: nums = [3,2,2,3], val = 3
+// Output: 2, nums = [2,2,_,_]
