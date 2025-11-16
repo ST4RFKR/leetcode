891ca7e9bbdd886d9d658a8536de2dcf5d8a2642
ST4RFKR <nikelodiym1210@gmail.com>
2025-11-16 14:22:03 +0200

    268. Missing Number.js

---
+++ 268. Missing Number.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+    let sum = 0;
+    let maxSum = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+    }
+    for (let i = 0; i < nums.length + 1; i++) {
+        maxSum += i
+    }
+    return maxSum - sum
+};
+
+var missingNumber2 = function (nums) {
+    const sortNumbers = [...nums].sort((a, b) => a - b)
+
+
+    for (let i = 0; i < sortNumbers.length; i++) {
+        if (sortNumbers[i] !== i) {
+            return i
+        }
+    }
+    return nums.length
+};
+console.log(missingNumber2([9, 6, 4, 2, 3, 5, 7, 0, 1]))
