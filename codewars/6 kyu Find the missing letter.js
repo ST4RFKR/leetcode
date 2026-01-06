52698fc60c6ca740ba20988cad22403053ca121f
ST4RFKR <nikelodiym1210@gmail.com>
2026-01-06 19:58:06 +0200

    6 kyu Find the missing letter.js

---
+++ codewars/6 kyu Find the missing letter.js
@@ -0,0 +1,8 @@
+function findMissingLetter(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].charCodeAt() !== array[i + 1].charCodeAt() - 1) {
+      return String.fromCharCode(array[i].charCodeAt() + 1);
+    }
+  }
+  return false;
+}
