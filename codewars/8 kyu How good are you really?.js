77727091921eddd1e35fb8767370e11219164d93
Evg_S <60222941+ST4RFKR@users.noreply.github.com>
2026-01-07 18:41:21 +0200

    Delete codewars/8 kyu How good are you really?.js

--- codewars/8 kyu How good are you really?.js
+++
@@ -1,6 +0,0 @@
-function betterThanAverage(classPoints, yourPoints) {
-  const avarageSum =
-    classPoints.reduce((a, b) => a + b, 0) / classPoints.length;
-  console.log(avarageSum);
-  return avarageSum < yourPoints;
-}
