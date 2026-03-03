b8fa504b0941d0f6df938858d9e0ec6ec7c1aada
ST4RFKR <nikelodiym1210@gmail.com>
2026-03-03 11:41:34 +0200

    6 kyu Counting Duplicates

---
+++ codewars/6 kyu Counting Duplicates.js
@@ -0,0 +1,32 @@
+// function duplicateCount(text) {
+//   const map = new Map();
+//   let count = 0;
+//   const normalizeText = text.toLowerCase();
+
+//   for (let ch of normalizeText) {
+//     map.set(ch, (map.get(ch) || 0) + 1);
+//   }
+
+//   for (let [_, value] of map) {
+//     if (value >= 2) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// В одном цыкле
+function duplicateCount(text) {
+  const map = new Map();
+  let count = 0;
+  const normalizeText = text.toLowerCase();
+
+  for (let ch of normalizeText) {
+    map.set(ch, (map.get(ch) || 0) + 1);
+    if (map.get(ch) === 2) {
+      count++;
+    }
+  }
+
+  return count;
+}
