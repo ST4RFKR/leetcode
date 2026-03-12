2f036b25c178c619f5bc5d8b57817b09152c1c17
ST4RFKR <nikelodiym1210@gmail.com>
2026-03-12 16:17:14 +0200

    7 kyu Sum of a sequence.js

---
+++ codewars/7 kyu Sum of a sequence.js
@@ -0,0 +1,13 @@
+const sequenceSum = (begin, end, step) => {
+  if (begin > end) {
+    return 0;
+  }
+
+  let res = 0;
+
+  for (let i = begin; i <= end; i += step) {
+    res += i;
+  }
+
+  return res;
+};
