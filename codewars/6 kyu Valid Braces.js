fcbcc2583a48a300e3c02941eb69a3ae3d2f1069
ST4RFKR <nikelodiym1210@gmail.com>
2026-03-17 15:32:06 +0200

    6 kyu Valid Braces.js

---
+++ codewars/6 kyu Valid Braces.js
@@ -0,0 +1,22 @@
+function validBraces(braces) {
+  const stack = [];
+  const matrix = {
+    "{": "}",
+    "[": "]",
+    "(": ")",
+  };
+  for (const brace of braces) {
+    if (matrix[brace]) {
+      stack.push(brace);
+    } else {
+      const lastBrace = stack.pop(brace);
+
+      if (matrix[lastBrace] !== brace) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
+
+validBraces("({{[]}})");
